fdc9427849fcbf94fc57756262cf1b223c826506
Kevin Ye <ranchonpizza24@gmail.com>
2026-02-10 13:34:08 -0800

    feat: parseInput function initial implementation

--- hw1/simpleshell.c
+++ hw1/simpleshell.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-void parseInput();
+int parseInput(char * input, char splitWord[][500], int maxWords);
 void changeDirectories();
 void executeCommand();
 
 int main() {
-    printf("Hello World\n");
+    char input[] = "Hello World";
+    int maxWords = 10;
+    char splitWord[maxWords][500];
+    int numElements = parseInput(input, splitWord, maxWords);
+    printf("%d\n", numElements);
     return 0;
+}
+
+int parseInput(char * input, char splitWord[][500], int maxWords) {
+    char * token = strtok(input, " ");
+    int i = 0;
+    while (token != NULL & i < maxWords) {
+        printf("%s\n", token);
+        strcpy(splitWord[i], token);
+        i++;
+        token = strtok(NULL, " ");
+    }
+    return i;
+}
+
+void changeDirectories() {
+    return;
+}
+
+void executeCommand() {
+    return;
 }
