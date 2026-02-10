9114d3d22c87b20c126e648f3921b069ce6f4c0e
Kevin Ye <ranchonpizza24@gmail.com>
2026-02-10 15:53:00 -0800

    feat: add implementation of getting netid and current working dir

--- hw1/simpleshell.c
+++ hw1/simpleshell.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int parseInput(char * input, char splitWord[][500], int maxWords);
 void changeDirectories();
 void executeCommand();
 
 int main() {
-    char input[] = "Hello World";
+    char netid[] = "kye";
+    char cwd[500];
+    getcwd(cwd, sizeof(cwd));
+
     int maxWords = 10;
     char splitWord[maxWords][500];
-    int numElements = parseInput(input, splitWord, maxWords);
-    printf("%d\n", numElements);
+    memset(splitWord, 0, sizeof(splitWord));
+
+
     return 0;
 }
 
@@ -18,7 +23,6 @@ int parseInput(char * input, char splitWord[][500], int maxWords) {
     char * token = strtok(input, " ");
     int i = 0;
     while (token != NULL & i < maxWords) {
-        printf("%s\n", token);
         strcpy(splitWord[i], token);
         i++;
         token = strtok(NULL, " ");
