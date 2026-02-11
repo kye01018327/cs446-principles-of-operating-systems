87444e4cd8760d9e3f4dfb0614b123119b4ff5c1
Kevin Ye <ranchonpizza24@gmail.com>
2026-02-10 23:17:24 -0800

    feat: expand check cases, cd, exit, if redirection...

--- hw1/simpleshell.c
+++ hw1/simpleshell.c
@@ -16,17 +16,66 @@ int main() {
     memset(splitWord, 0, sizeof(splitWord));
 
     char userInput[500];
+    int hasInputRedirection = 0;
+    int hasOutputRedirection = 0;
+    int hasPipe = 0;
 
     while (1) {
         printf("%s:%s$ ", netid, cwd);
         scanf(" %[^\n]", userInput);
         printf("User Input: %s\n", userInput);
         parseInput(userInput, splitWord, maxWords);
+
+        char * firstElement = splitWord[0];
+        // if first element is cd, call changeDirectories
+        if (strcmp(firstElement, "cd") == 0) {
+            changeDirectories();
+        }
+        // if first element is exit, stop looping and return from main with 0 return value
+        else if (strcmp(firstElement, "exit") == 0) {
+            return 0;
+        }
+        // check if contains input redirection (<) or output redirection (>)
+        for (int i = 0; i < maxWords; i++) {
+            if (strcmp(splitWord[i], "<") == 0) {
+                hasInputRedirection = 1;
+                break;
+            }
+            if (strcmp(splitWord[i], ">") == 0) {
+                hasOutputRedirection = 1;
+                break;
+            }
+            if (strcmp(splitWord[i], "|") == 0) {
+                hasPipe = 1;
+                break;
+            }
+        }
+        if (hasInputRedirection) {
+            printf("Input redirection detected (<)\n");
+            hasInputRedirection = 0;
+        }
+        else if (hasOutputRedirection) {
+            printf("Output redirection detected (>)\n");
+            hasOutputRedirection = 0;
+        }
+        else if (hasPipe) {
+            printf("Pipe redirection detected (|)\n");
+            hasPipe = 0;
+        }
+        // if no redirection (single CLI command)
+        else {
+            // pass
+        }
+
+
+        // debug
         printf("Parsed Input:");
         for (int i = 0; i < maxWords; i++) {
             printf(" %s", splitWord[i]);
         }
         printf("\n");
+
+        // reset
         memset(splitWord, 0, sizeof(splitWord));
     }
 
@@ -46,6 +95,7 @@ int parseInput(char * input, char splitWord[][500], int maxWords) {
 }
 
 void changeDirectories() {
+    printf("changeDirectories called\n");
     return;
 }
 
