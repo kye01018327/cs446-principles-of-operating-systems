7f37433bb1fd7e50499404c283c8aef2dc770ced
Kevin Ye <ranchonpizza24@gmail.com>
2026-02-25 18:05:59 -0800

    chore: init modify_value

---
+++ hw3/modify_value.c
@@ -0,0 +1,7 @@
+#include "modify_value.h"
+void modify_value_up(int *value) {
+*value = *value + 1;
+}
+void modify_value_down(int *value) {
+*value = *value - 1;
+}
---
+++ hw3/modify_value.h
@@ -0,0 +1,5 @@
+#ifndef MODIFY_VALUE_H_
+#define MODIFY_VALUE_H_
+void modify_value_up(int *value);
+void modify_value_down(int *value);
+#endif
